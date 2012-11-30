31dc1bc129b99064cd76f3db621bac01aa0d19ba
Richard Wallace <rwallace@thewallacepack.net>
2012-11-30 11:34:13 -0700

    fix repl output capturing

--- src/main/scala/com/atlassian/ecosystem/marvin/ScalaServlet.scala
+++ src/main/scala/com/atlassian/ecosystem/marvin/ScalaServlet.scala
@@ -34,45 +34,37 @@ private[this] class ScalaInterpreter(key: String, room: Room) {
 
   def interpret(code: String): Unit = actor ! code
 
-  private[this] val stdOut = System.out
-  private[this] val stdErr = System.err
-  private[this] val conOut = new ByteArrayOutputStream
-  private[this] val conOutStream = new PrintStream(conOut)
-
   implicit val SingleThreadedStrategy = Strategy.Executor(Executors.newSingleThreadExecutor)
 
+  val out = new ByteArrayOutputStream
   private[this] lazy val si = {
     println("building IMain instance")
     val settings = new scala.tools.nsc.Settings(null)
     settings.usejavacp.value = true
     settings.deprecation.value = true
     settings.YdepMethTpes.value = true
-    val si = new IMain(settings) { 
+    val si = new IMain(settings, new scala.tools.nsc.NewLinePrintWriter(new OutputStreamWriter(out), true)) { 
       override def parentClassLoader = Thread.currentThread.getContextClassLoader 
     }
     si.quietImport("scalaz._")
     si.quietImport("Scalaz._")
     si
   }
-  private[this] def interpret_(code: String) = 
-    try {
-      System setOut conOutStream
-      System setErr conOutStream
-      println("interpreting '%s'".format(code))
-      si.interpret(code)
-    } finally {
-      System setOut stdOut
-      System setErr stdErr
-      conOut.flush
-      conOut.reset
-    }
 
+  private[this] def interpret_[A](code: String)(f: (Result, String) => A) =
+    try {
+      val r = si.interpret(code)
+      f(r, out.toString)
+    } finally {
+      out.flush
+      out.reset
+    }
   private[this] val actor = Actor[String] { code ⇒
-    Hipchat.sendMessage(key, Left(room.id), interpret_(code) match {
-      case Success => println("successfully interpreted"); conOut.toString.replaceAll("(?m:^res[0-9]+: )", "") // + "\n" + iout.toString.replaceAll("(?m:^res[0-9]+: )", "")
-      case Error => println("error interpreting"); conOut.toString.replaceAll("^<console>:[0-9]+: ", "")
+    Hipchat.sendMessage(key, Left(room.id), interpret_(code)((r, out) ⇒ r match {
+      case Success => println("successfully interpreted"); out.replaceAll("(?m:^res[0-9]+: )", "") // + "\n" + iout.toString.replaceAll("(?m:^res[0-9]+: )", "")
+      case Error => println("error interpreting"); out.replaceAll("^<console>:[0-9]+: ", "")
       case Incomplete => println("incomplete expression"); "error: unexpected EOF found, incomplete expression"
-    })
+    }))
   }
 }
 
